be9cc4f4a4660993cce0aa764a191c629d8aecd1
TannuKumari0889 <tannukumari9868@gmail.com>
2025-11-16 21:03:12 +0530

    Update Business_Overview.py

--- pages/Business_Overview.py
+++ pages/Business_Overview.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
+import numpy as np
 from data_tables import load_data
 sessions, pageviews, products, orders, items, refunds = load_data()
 
-#-kpis--------------
 total_orders=len(orders['order_id'])
+total_revenue=orders['price_usd'].sum()
+
+#-kpis--------------
+col1,col2=st.columns(2)
+col1.metirc['Total Orders',total_orders]
+col1.metirc['Total Revenue',total_revenue]
+
