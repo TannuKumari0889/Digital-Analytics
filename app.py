054c71133ca8f36b3ea560d2400e40b97ba19b65
TannuKumari0889 <tannukumari9868@gmail.com>
2025-11-16 20:05:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ st.set_page_config(page_title="Digital Analytics Dashboard", layout="wide")
 # ----------------------------
 # Sidebar Navigation
 # ----------------------------
-st.markdown("""
-<div class='cover-box'>
-    <div class='big-title'>📊 Digital Analytics Dashboard</div>
-    <p class='sub-title'>Welcome! Use the buttons below or the sidebar to navigate between pages.</p>
-</div>
-""", unsafe_allow_html=True)
+
 
 # ----------------------------
 # Navigation Buttons
