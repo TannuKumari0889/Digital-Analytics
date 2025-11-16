d1e3c6f131da0155ea1418bce2ecec2aa85e0b3c
TannuKumari0889 <tannukumari9868@gmail.com>
2025-11-16 20:58:21 +0530

    Update Business_Overview.py

--- pages/Business_Overview.py
+++ pages/Business_Overview.py
@@ -1,3 +1,6 @@
-import streamlit as st 
+import streamlit as st
 from data_tables import load_data
 sessions, pageviews, products, orders, items, refunds = load_data()
+
+#-kpis--------------
+total_orders=len(orders['order_id'])
