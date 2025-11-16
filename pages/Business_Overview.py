dfa46f25326089989669ce4ce62c00d2eecf0146
TannuKumari0889 <tannukumari9868@gmail.com>
2025-11-16 22:04:55 +0530

    Update Business_Overview.py

--- pages/Business_Overview.py
+++ pages/Business_Overview.py
@@ -15,9 +15,11 @@ AOV=orders['price_usd'].sum()/total_orders
 refund_rate=len(refunds['order_item_refund_id'])*100/total_orders
 cvr=total_orders*100/len(sessions['website_session_id'])
 total_customers=orders['user_id'].nunique()
+repeat_customers = (orders['user_id'].value_counts() > 1).sum()
+
 
 #-kpis--------------
-col1,col2,col3,col4,col5,col6,col7=st.columns(7)
+col1,col2,col3,col4,col5,col6,col7,col8=st.columns(8)
 col1.metric('Total Orders',total_orders)
 col2.metric('Total Revenue',total_revenue,"M")
 col3.metric('Total Profit',total_profit,"M")
@@ -25,5 +27,6 @@ col4.metric('Avg Order Value',round(AOV,2),"M")
 col5.metric('Refund Rate',round(refund_rate,2),"%")
 col6.metric('Coversion Rate',round(cvr,2),"%")
 col7.metric('Total Customers',total_customers)
+col8.metric('Repeat Customers',repeat_customers)
 
 
