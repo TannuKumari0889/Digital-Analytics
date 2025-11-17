42446802a149702824424eb55c0f1251b96fe2a7
TannuKumari0889 <tannukumari9868@gmail.com>
2025-11-17 11:45:03 +0530

    Update Marketing_Analysis.py

--- pages/Marketing_Analysis.py
+++ pages/Marketing_Analysis.py
@@ -1,3 +1,8 @@
 import streamlit as st 
+import plotly.express as px
+import pandas as pd
+import numpy as np
 from data_tables import load_data
 sessions, pageviews, products, orders, items, refunds = load_data()
+
+
