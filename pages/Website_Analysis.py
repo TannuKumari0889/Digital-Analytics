84c0c3981e56cc577c086abc70a1911e762cac82
TannuKumari0889 <tannukumari9868@gmail.com>
2025-11-17 13:43:51 +0530

    Update Website_Analysis.py

--- pages/Website_Analysis.py
+++ pages/Website_Analysis.py
@@ -5,6 +5,8 @@ import numpy as np
 from data_tables import load_data
 sessions, pageviews, products, orders, items, refunds = load_data()
 
+st.title('🌐Website Analysis')
+
 ## KPI's Calculations--------------------
 ##-----------------------------------------
 total_sessions=len(sessions['website_session_id'])
@@ -25,7 +27,7 @@ top_traffic_source=sessions['utm_source'].value_counts().idxmax()
 
 col1,col2,col3,col4,col5=st.columns(5)
 col1.metric('Total Sessions',total_sessions)
-col2.metric('Bounce Rate',bounce_rate)
+col2.metric('Bounce Rate',round(bounce_rate,2),"%")
 col3.metric('Top Entry Page',top_entry_page)
 col4.metric('Top Exit Page',top_exit_page)
 col5.metric('Top Traffic Source',top_traffic_source)
