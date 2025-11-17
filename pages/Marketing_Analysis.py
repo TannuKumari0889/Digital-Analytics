70e1348e2e4f69079bac0fd624b62a689dccbe8e
TannuKumari0889 <tannukumari9868@gmail.com>
2025-11-17 12:19:30 +0530

    Update Marketing_Analysis.py

--- pages/Marketing_Analysis.py
+++ pages/Marketing_Analysis.py
@@ -10,12 +10,16 @@ sessions, pageviews, products, orders, items, refunds = load_data()
 gsearch_sessions=sessions.loc[sessions['utm_source']=='gsearch','website_session_id'].count()
 gsearch_cvr = orders['website_session_id'].isin(sessions[sessions['utm_source']=='gsearch']['website_session_id']).sum() / gsearch_sessions*100
 
-repeat_customer=orders['user_id'].value_counts.
+total_customer=orders['user_id'].nunique()
+repeat_customer=(orders['user_id'].value_counts()>1).sum()
+repeat_customer_rate=round(repeat_customer/total_customer*100,2)
+
+
 
 
 
 col1,col2,col3,col4,col5,col6=st.columns(6)
 col1.metric('Gsearch CVR',round(gsearch_cvr,2))
-col2.metric()
-
+col2.metric('Repeat Customer Rate',repeat_customer_rate,"%")
+col3.metric('',)
 
